1e4ab24417f4a440a21ccf0c2bf7b4530f925b98
Thomas Ritaine <thomas.ritaine@outlook.com>
2023-04-10 12:52:36 -1000

    Add Starship config

--- bash_custom/init.sh
+++ bash_custom/init.sh
@@ -6,6 +6,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Load bash_aliases
 . "$SCRIPT_DIR/.bash_aliases"
 
+source "$SCRIPT_DIR/utils/distro.sh"
+
 # Add Starship
 export STARSHIP_CONFIG="$SCRIPT_DIR/../.config/starship.toml"
 eval "$(starship init bash)"
---
+++ bash_custom/utils/distro.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# find out which distribution we are running on
+if [[ $(find /etc -maxdepth 1 -type f -name "*-release" | wc -l) -gt 0 ]]; then
+  _distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
+elif [[ -f "/System/Library/CoreServices/SystemVersion.plist" ]]; then
+  _distro="macos"
+fi
+
+# set an icon based on the distro
+# make sure your font is compatible with https://github.com/lukas-w/font-logos
+case $_distro in
+    *kali*)                  ICON="ﴣ";;
+    *arch*)                  ICON="";;
+    *debian*)                ICON="";;
+    *raspbian*)              ICON="";;
+    *ubuntu*)                ICON="";;
+    *elementary*)            ICON="";;
+    *fedora*)                ICON="";;
+    *coreos*)                ICON="";;
+    *gentoo*)                ICON="";;
+    *mageia*)                ICON="";;
+    *centos*)                ICON="";;
+    *opensuse*|*tumbleweed*) ICON="";;
+    *sabayon*)               ICON="";;
+    *slackware*)             ICON="";;
+    *linuxmint*)             ICON="";;
+    *alpine*)                ICON="";;
+    *aosc*)                  ICON="";;
+    *nixos*)                 ICON="";;
+    *devuan*)                ICON="";;
+    *manjaro*)               ICON="";;
+    *rhel*)                  ICON="";;
+    *macos*)                 ICON="";;
+    *)                       ICON="";;
+esac
+
+export STARSHIP_DISTRO="$ICON"
